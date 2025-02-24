a6309ffe85b93e086be3caf63fe832979772e5dc
Om Patel <ompatel2019@gmail.com>
2025-02-24 12:30:19 +1100

    Lighthouse Changes - 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './animations.css'
-import './sliders.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx'
 
