808d06a2518ae37d234e6bd3c2b9f1ed3e750314
Om Patel <ompatel2019@gmail.com>
2025-08-25 12:08:33 +1000

    Refactor Navbar Configuration: Clean up and organize menu item definitions for improved readability and maintainability

--- lib/config/navbarConfig.tsx
+++ lib/config/navbarConfig.tsx
@@ -1,21 +1,19 @@
 // lib/config/navbarConfig.tsx
 
 export type MenuItem = {
-    name: string;
-    to: string;
-  };
-  
-  export const menuItems: MenuItem[] = [
-    { name: "Home", to: "/" },
-    { name: "Specialty", to: "/specialty" },
-    { name: "Why Choose Us", to: "/why-choose-us" },
-    { name: "About Us", to: "/about-us" },
-    { name: "Pricing", to: "/pricing" },
-    { name: "Blogs", to: "/blogs" },
-  ];
-  
-  export const fullNavbarLinks: MenuItem[] = [
-    ...menuItems,
-    { name: "Enrol Now", to: "/contact-us" },
-  ];
-  
+  name: string;
+  to: string;
+};
+
+export const menuItems: MenuItem[] = [
+  { name: "Home", to: "/" },
+  { name: "Specialty", to: "/specialty" },
+  { name: "Why Choose Us", to: "/why-choose-us" },
+  { name: "About Us", to: "/about-us" },
+  { name: "Pricing", to: "/pricing" },
+];
+
+export const fullNavbarLinks: MenuItem[] = [
+  ...menuItems,
+  { name: "Enrol Now", to: "/contact-us" },
+];
